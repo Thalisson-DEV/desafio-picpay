8c75fc553e5bcd0fd0d21664b33adf6e43443afd
Thalisson. <126502431+Thalisson-DEV@users.noreply.github.com>
2026-01-10 18:25:18 -0300

    feat(User): Mapper e Repository adicionados

---
+++ src/main/java/com/desafiopicpay/mappers/UserMapper.java
@@ -0,0 +1,42 @@
+package com.desafiopicpay.mappers;
+
+import com.desafiopicpay.domain.role.Role;
+import com.desafiopicpay.domain.user.User;
+import com.desafiopicpay.dtos.PaginatedUsersResponseDTO;
+import com.desafiopicpay.dtos.UserRequestDTO;
+import com.desafiopicpay.dtos.UserResponseDTO;
+import lombok.NonNull;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
+import org.springframework.data.domain.Page;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    User toUserEntity(UserRequestDTO dto);
+
+    UserResponseDTO toUserResponseDTO(User entity);
+
+    @Mapping(target = "id", ignore = true)
+    void updateUserFromDTO(UserRequestDTO dto, @MappingTarget User entity);
+
+    default PaginatedUsersResponseDTO toPaginatedResponseDTO(Page<@NonNull User> page) {
+        return new PaginatedUsersResponseDTO(
+                page.getContent()
+                        .stream()
+                        .map(this::toUserResponseDTO)
+                        .toList(),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages()
+        );
+    }
+}
---
+++ src/main/java/com/desafiopicpay/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.desafiopicpay.repositories;
+
+import com.desafiopicpay.domain.user.User;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<@NonNull User, @NonNull Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
