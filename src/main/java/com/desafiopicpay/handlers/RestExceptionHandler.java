30b25aff71a1a62dae92a73753cbbb5d04141264
Thalisson. <126502431+Thalisson-DEV@users.noreply.github.com>
2026-01-10 18:29:02 -0300

    feat(handler): RestExceptionHandler adicionado

---
+++ src/main/java/com/desafiopicpay/handlers/RestExceptionHandler.java
@@ -0,0 +1,79 @@
+package com.desafiopicpay.handlers;
+
+import com.desafiopicpay.dtos.ExceptionResponseDTO;
+import com.desafiopicpay.exceptions.InvalidOperationException;
+import com.desafiopicpay.exceptions.ServiceUnivaliablleException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.NonNull;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<@NonNull ExceptionResponseDTO> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        ExceptionResponseDTO exceptionResponse = new ExceptionResponseDTO(
+                LocalDateTime.now(),
+                e.getMessage(),
+                "/api/v1/users",
+                status
+        );
+
+        return ResponseEntity.badRequest().body(exceptionResponse);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<@NonNull ExceptionResponseDTO> handleEntityNotFoundException() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(InvalidOperationException.class)
+    public ResponseEntity<@NonNull ExceptionResponseDTO> handleInvalidOperationException(InvalidOperationException e) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        ExceptionResponseDTO exceptionResponse = new ExceptionResponseDTO(
+                LocalDateTime.now(),
+                e.getMessage(),
+                "/api/v1/transactions",
+                status
+        );
+
+        return ResponseEntity.badRequest().body(exceptionResponse);
+    }
+
+    @ExceptionHandler(ServiceUnivaliablleException.class)
+    public ResponseEntity<@NonNull ExceptionResponseDTO> handleServiceUnivaliablleException(ServiceUnivaliablleException e) {
+        HttpStatus status = HttpStatus.BAD_GATEWAY;
+
+        ExceptionResponseDTO exceptionResponse = new ExceptionResponseDTO(
+                LocalDateTime.now(),
+                e.getMessage(),
+                null,
+                status
+        );
+
+        return ResponseEntity.internalServerError().body(exceptionResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<@NonNull ExceptionResponseDTO> handleGeneralException(Exception e) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        ExceptionResponseDTO exceptionResponse = new ExceptionResponseDTO(
+                LocalDateTime.now(),
+                e.getMessage(),
+                null,
+                status
+        );
+
+        return ResponseEntity.internalServerError().body(exceptionResponse);
+    }
+}
