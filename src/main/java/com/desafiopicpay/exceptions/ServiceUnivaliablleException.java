c574e12f4e94e7eec4310475efb567d449fedd0f
Thalisson. <126502431+Thalisson-DEV@users.noreply.github.com>
2026-01-10 18:27:57 -0300

    chore(exceptions): exceções de negócio personalizadas

---
+++ src/main/java/com/desafiopicpay/exceptions/ServiceUnivaliablleException.java
@@ -0,0 +1,7 @@
+package com.desafiopicpay.exceptions;
+
+public class ServiceUnivaliablleException extends RuntimeException {
+    public ServiceUnivaliablleException(String message) {
+        super(message);
+    }
+}
