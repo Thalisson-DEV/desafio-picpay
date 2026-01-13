d78eb2db2458e3fe1e507403eeb36de53b663953
Thalisson. <126502431+Thalisson-DEV@users.noreply.github.com>
2026-01-13 12:20:11 -0300

    feat(auth): dtos de login adicionados

---
+++ src/main/java/com/desafiopicpay/dtos/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.desafiopicpay.dtos;
+
+public record LoginRequestDTO(String email, String password) {
+}
---
+++ src/main/java/com/desafiopicpay/dtos/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package com.desafiopicpay.dtos;
+
+public record LoginResponseDTO(String accessToken, Long expiresIn) {
+}
